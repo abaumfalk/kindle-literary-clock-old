2c3b055a7e7f182286cff33b733004fef97327fc
Arno Baumfalk <a.baumfalk@web.de>
2024-01-14 17:09:23 +0100

    WIP

--- quote_to_image/ABa/cffi/test.py
+++ quote_to_image/ABa/cffi/test.py
@@ -2,7 +2,7 @@
 import cairocffi
 import pangocffi
 import pangocairocffi
-from pangocffi import units_from_double
+from pangocffi import units_from_double, units_to_double
 
 WIDTH = 200
 HEIGHT = 400
@@ -24,6 +24,13 @@ layout.apply_markup(f"This is a <b>long</b> example-text, which should be wrappe
                     '<span foreground="blue" size="x-large">Blue text</span> is <i>cool</i>!\n'
                     'Now we have all flexibility needed to display the time in bold letters, such as <b>11:43</b>, '
                     'anywhere in the text!')
+font = pangocffi.FontDescription()
+font.size = 60
+layout.font_description = font
+
+inc, logical = layout.get_extents()
+print(f"inc rect: {units_to_double(inc.width)} x {units_to_double(inc.height)}")
+print(f"logical rect: {units_to_double(logical.width)} x {units_to_double(logical.height)}")
 
 pangocairocffi.show_layout(context, layout)
 surface.write_to_png("test.png")
